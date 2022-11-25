4f456675e5a32d769d0f833c1b3ee3000667c966
andreasavignano02 <asavignano2002@gmail.com>
2022-11-25 15:41:47 +0100

    start

---
+++ U1-W1-D5/U1-W1-D5/Contribuente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U1_W1_D5
+{
+    internal class Contribuente
+    {
+        private static string _Name;
+        private static string _Surname;
+        private static string _DataNascita;
+        private static string _CodiceFiscale;
+        private static string _Sesso;
+        private static string _ComuneDiResidenza;
+        private static double _RedditoAnnuale;
+        public Contribuente() { string _Name; string _Surname; string _DataNascita; }
+        private static void Tasse()
+        {
+            Console.WriteLine("=============================");
+            Console.WriteLine("Inserisci il tuo nome:");
+        }
+    }
+}
