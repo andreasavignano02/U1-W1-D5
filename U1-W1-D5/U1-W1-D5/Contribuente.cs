46527ced032dafe594858869528f95da4505408a
andreasavignano02 <asavignano2002@gmail.com>
2022-11-26 16:21:30 +0100

    add-imposta

--- U1-W1-D5/U1-W1-D5/Contribuente.cs
+++ U1-W1-D5/U1-W1-D5/Contribuente.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,8 +30,19 @@ namespace U1_W1_D5
             _DataNascita = DateTime.Parse(Console.ReadLine());
             Console.WriteLine("Inserisci il tuo sesso M/F");            
             Sesso();
-            
-            //Console.WriteLine($"{_DataNascita.Day}/{_DataNascita.Month}/{_DataNascita.Year}");
+            Console.WriteLine("inserisci il tuo codice fiscale:");
+            _CodiceFiscale= Console.ReadLine();
+            Console.WriteLine("Inserisci il tuo comune di residenza");
+            _ComuneDiResidenza = Console.ReadLine();
+            Console.WriteLine("Inserisci il tuo reddito lordo annuale:");
+            _RedditoAnnuale = double.Parse(Console.ReadLine());
+            Console.WriteLine("==========================");
+            Console.WriteLine($"Contribuente : {_Name}{_Surname}");
+            Console.WriteLine($"nato il {_DataNascita.Day}/{_DataNascita.Month}/{_DataNascita.Year} ({_Sesso})");
+            Console.WriteLine($"residente in {_ComuneDiResidenza}");
+            Console.WriteLine($"codice fiscale: {_CodiceFiscale}");
+            Console.WriteLine($"Reddito dichiarato: {_RedditoAnnuale}");
+            imposta();
             Console.ReadLine();
         }
         private static void Sesso()
@@ -40,6 +52,36 @@ namespace U1_W1_D5
             {
                 Console.WriteLine("inserisci un sesso valido");
                 Tasse();
+            }
+        }
+        private static void imposta()
+        {
+            double impostadapagare;
+            if (_RedditoAnnuale >= 0 && _RedditoAnnuale <= 15.000)
+            {
+                impostadapagare  = (_RedditoAnnuale * 23) / 100;
+                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+
+            }
+            else if (_RedditoAnnuale >= 15.001 && _RedditoAnnuale <= 28000) 
+            {
+                impostadapagare = 3.450 + ((_RedditoAnnuale * 27) / 100);
+                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+            }
+            else if (_RedditoAnnuale >= 28.001 && _RedditoAnnuale <= 55.000)
+            {
+                impostadapagare = 6.960 + ((_RedditoAnnuale * 38) / 100);
+                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+            }
+            else if (_RedditoAnnuale >= 55.001 && _RedditoAnnuale <= 75.000) 
+            {
+                impostadapagare = 17.220 + ((_RedditoAnnuale * 41) / 100);
+                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+            }
+            else if (_RedditoAnnuale >= 75.001)
+            {
+                impostadapagare = 25.420 + ((_RedditoAnnuale * 43) / 100);
+                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
             }
         }
     }
