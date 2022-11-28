94f9260e3fdebbcacab188038809d5fbec62a7f8
andreasavignano02 <asavignano2002@gmail.com>
2022-11-28 09:49:17 +0100

    fix-all

--- U1-W1-D5/U1-W1-D5/Contribuente.cs
+++ U1-W1-D5/U1-W1-D5/Contribuente.cs
@@ -37,7 +37,7 @@ namespace U1_W1_D5
             Console.WriteLine("Inserisci il tuo reddito lordo annuale:");
             _RedditoAnnuale = double.Parse(Console.ReadLine());
             Console.WriteLine("==========================");
-            Console.WriteLine($"Contribuente : {_Name}{_Surname}");
+            Console.WriteLine($"Contribuente : {_Name} {_Surname}");
             Console.WriteLine($"nato il {_DataNascita.Day}/{_DataNascita.Month}/{_DataNascita.Year} ({_Sesso})");
             Console.WriteLine($"residente in {_ComuneDiResidenza}");
             Console.WriteLine($"codice fiscale: {_CodiceFiscale}");
@@ -60,28 +60,28 @@ namespace U1_W1_D5
             if (_RedditoAnnuale >= 0 && _RedditoAnnuale <= 15.000)
             {
                 impostadapagare  = (_RedditoAnnuale * 23) / 100;
-                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+                Console.WriteLine($"IMPOSTA DA VERSARE :{impostadapagare}");
 
             }
             else if (_RedditoAnnuale >= 15.001 && _RedditoAnnuale <= 28000) 
             {
                 impostadapagare = 3.450 + ((_RedditoAnnuale * 27) / 100);
-                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+                Console.WriteLine($"IMPOSTA DA VERSARE: {impostadapagare}");
             }
             else if (_RedditoAnnuale >= 28.001 && _RedditoAnnuale <= 55.000)
             {
                 impostadapagare = 6.960 + ((_RedditoAnnuale * 38) / 100);
-                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+                Console.WriteLine($"IMPOSTA DA VERSARE: {impostadapagare}");
             }
             else if (_RedditoAnnuale >= 55.001 && _RedditoAnnuale <= 75.000) 
             {
                 impostadapagare = 17.220 + ((_RedditoAnnuale * 41) / 100);
-                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+                Console.WriteLine($"IMPOSTA DA VERSARE: {impostadapagare}");
             }
             else if (_RedditoAnnuale >= 75.001)
             {
                 impostadapagare = 25.420 + ((_RedditoAnnuale * 43) / 100);
-                Console.WriteLine($"IMPOSTA DA VERSARE {impostadapagare}");
+                Console.WriteLine($"IMPOSTA DA VERSARE: {impostadapagare}");
             }
         }
     }
